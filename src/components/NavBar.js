b09f9f012bab0c3ee36e9ffd2a33e9a3de22dd79
Jessy-Jones <93819297+Jessy-Jones@users.noreply.github.com>
2024-06-11 14:25:47 +0000

    Baby Steps in React. Reacts Fun Facts Page

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="../logo192.png" alt="React Logo"/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project1</h4>
+        </nav>
+    )
+}
